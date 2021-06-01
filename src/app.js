3d9e9faaff2ec0260e7725e95ae61a4ab2b71d2f
jiggy2204 <jignacio2204@gmail.com>
2021-06-01 19:31:20 -0400

    update app.js

--- src/app.js
+++ src/app.js
@@ -3,15 +3,13 @@ const express = require("express");
 const morgan = require("morgan");
 const cors = require("cors");
 const helmet = require("helmet");
-
 const { NODE_ENV } = require("./config");
-const { default: knex } = require("knex");
-
-const foldersRouter = require("./folders/folders-router");
-const notesRouter = require("./notes/notes-router");
+const foldersRouter = require("./folders-router");
+const notesRouter = require("./notes-router");
 
 const app = express();
-const morganOption = NODE_ENV === "production";
+
+const morganOption = NODE_ENV === "production" ? "tiny" : "common";
 
 app.use(morgan(morganOption));
 app.use(helmet());
@@ -20,29 +18,14 @@ app.use(cors());
 app.use("/api/folders", foldersRouter);
 app.use("/api/notes", notesRouter);
 
-//GET root page, send back 'Hello, world!' on web page
-// app.get("/", (req, res) => {
-//   res.send("Hello, world!");
-// });
-
-// app.get("/api/folders", (req, res) => {
-//   console.log(res.send("Retrieved folders"));
-// });
-
-// app.get("/api/notes", (req, res) => {
-//   console.log(res.send("Retrieved notes"));
-// });
-
-//Hide error message from users and outsiders
 app.use(function errorHandler(error, req, res, next) {
   let response;
   if (NODE_ENV === "production") {
     response = { error: { message: "server error" } };
   } else {
-    console.error(error);
+    console.log(error);
     response = { message: error.message, error };
   }
-
   res.status(500).json(response);
 });
 
