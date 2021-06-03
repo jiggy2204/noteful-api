fd5d55da3a2678967938b1214b85236b45f7b7a3
jiggy2204 <jignacio2204@gmail.com>
2021-06-03 02:42:49 -0400

    updated api

--- postgrator-config.js
+++ postgrator-config.js
@@ -1,13 +1,13 @@
-require('dotenv').config();
-
 //This will set the correct SSL flag for heroku, without breaking your app for local development
 const pg = require('pg');
 pg.defaults.ssl = process.env.NODE_ENV === "production" ? { rejectUnauthorized: false } : false;
 
+require('dotenv').config();
+
 module.exports = {
-  "migrationDirectory": "migrations",
-  "driver": "pg",
-  "connectionString": (process.env.NODE_ENV === 'test')
-    ? process.env.TEST_DATABASE_URL
-    : process.env.DATABASE_URL,
+    "migrationsDirectory": "migrations",
+    "driver": "pg",
+    "connectionString": (process.env.NODE_ENV === 'test')
+        ? process.env.TEST_DATABASE_URL
+        : process.env.DATABASE_URL,
 }
